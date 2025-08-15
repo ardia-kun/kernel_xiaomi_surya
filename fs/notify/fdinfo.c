411f07e51158b309adfce0ab66ba115feeaa2fde
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2025-08-15 21:46:38 +0700

    Update fdinfo.c

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -119,7 +119,7 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 			}
 			seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
 			   inode_mark->wd, path.dentry->d_inode->i_ino, path.dentry->d_inode->i_sb->s_dev,
-			show_mark_fhandle(m, path.dentry->d_inode);
+			show_mark_fhandle(m, path.dentry->d_inode));
 			seq_putc(m, '\n');
 			iput(inode);
 			path_put(&path);
@@ -132,7 +132,7 @@ out_seq_printf:
 #endif
 		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
 			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
-			   mask, mark->ignored_mask);
+			  	mark->ignored_mask);
 		show_mark_fhandle(m, inode);
 		seq_putc(m, '\n');
 		iput(inode);
