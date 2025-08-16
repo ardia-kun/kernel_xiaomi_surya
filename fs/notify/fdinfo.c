322fcdfb68fe5d66dd0cc8e2c6f02a39883c163e
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2025-08-16 22:23:47 +0700

    Update fdinfo.c

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -84,6 +84,8 @@ static void show_mark_fhandle(struct seq_file *m, struct inode *inode)
 }
 #endif
 
+#ifdef CONFIG_INOTIFY_USER
+
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark, struct file *file)
 #else
@@ -116,11 +118,11 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 				goto out_free_pathname;
 			}
 			seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
-			    inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
-			  	mark->ignored_mask);
-		    show_mark_fhandle(m, inode);
-		    seq_putc(m, '\n');
-		    iput(inode);
+			   inode_mark->wd, path.dentry->d_inode->i_ino, path.dentry->d_inode->i_sb->s_dev,
+			   inotify_mark_user_mask(mark));
+			show_mark_fhandle(m, path.dentry->d_inode);
+			seq_putc(m, '\n');
+			iput(inode);
 			path_put(&path);
 			kfree(pathname);
 			return;
@@ -129,6 +131,21 @@ out_free_pathname:
 		}
 out_seq_printf:
 #endif
+		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
+			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
+			   inotify_mark_user_mask(mark));
+		show_mark_fhandle(m, inode);
+		seq_putc(m, '\n');
+		iput(inode);
+	}
+}
+
+void inotify_show_fdinfo(struct seq_file *m, struct file *f)
+{
+	show_fdinfo(m, f, inotify_fdinfo);
+}
+
+#endif /* CONFIG_INOTIFY_USER */
 
 #ifdef CONFIG_FANOTIFY
 
